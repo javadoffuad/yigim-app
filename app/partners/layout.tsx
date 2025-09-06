8425e6f540612bed72c423e7c1a1bd9ab2015030
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-09-06 23:19:20 +0300

    chore (app): add page layour

---
+++ app/partners/layout.tsx
@@ -0,0 +1,36 @@
+import type { Metadata } from "next";
+import { Geist, Geist_Mono } from "next/font/google";
+import "../../app/globals.css";
+import Header from '@/components/header';
+
+const geistSans = Geist({
+  variable: "--font-geist-sans",
+  subsets: ["latin"],
+});
+
+const geistMono = Geist_Mono({
+  variable: "--font-geist-mono",
+  subsets: ["latin"],
+});
+
+export const metadata: Metadata = {
+  title: "Yigim Partners",
+  description: "Yigim partners page description",
+};
+
+export default function RootLayout({
+  children,
+}: Readonly<{
+  children: React.ReactNode;
+}>) {
+  return (
+    <html lang="en">
+      <body
+        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      >
+        <Header></Header>
+        {children}
+      </body>
+    </html>
+  );
+}
