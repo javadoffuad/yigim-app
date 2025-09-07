609b4ccfb61acdc04817dcfdec4daf06cdb2a2c9
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-09-08 01:02:19 +0300

    chore(app): update faq, header

--- app/components/faq/faq.tsx
+++ app/components/faq/faq.tsx
@@ -7,20 +7,34 @@ interface Faq {
 
 export default function Faq({items}: {items: Faq[]}) {
     return (
-        <div className={styles.faqList}>
-            {items.map(item => (
-            <div className={styles.faqItem}>
-                <button className={styles.faqQuestion}>
-                    {item.question}
-                    <svg className={styles.faqIcon} width="24" height="24" viewBox="0 0 24 24" fill="none">
-                        <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
-                    </svg>
-                </button>
-                <div className={styles.faqAnswer}>
-                    <p>{item.answer}</p>
+        <section className={styles["faq-section"]}>
+            <div className={styles["faq-content"]}>
+                <div className={styles["faq-left"]}>
+                    <div className={styles["faq-header"]}>
+                        <span className={styles["section-label"]}>FREQUENTLY ASKED QUESTIONS</span>
+                        <h2 className={styles["faq-title"]}>Have<br />questions in<br />mind? Let us<br />answer it</h2>
+                        <p className={styles["faq-contact"]}>
+                            Don't answer your question?<br />
+                            contact us at <span className={styles["contact-email"]}>contact@yigim.az</span>
+                        </p>
+                    </div>
+                </div>
+                <div className={styles.faqList}>
+                    {items.map(item => (
+                        <div className={styles.faqItem}>
+                        <button className={styles.faqQuestion}>
+                            {item.question}
+                            <svg className={styles.faqIcon} width="24" height="24" viewBox="0 0 24 24" fill="none">
+                                <path d="M12 5V19M5 12H19" stroke="currentColor" strokeWidth="2" strokeLinecap="round" strokeLinejoin="round"/>
+                            </svg>
+                        </button>
+                        <div className={styles.faqAnswer}>
+                            <p>{item.answer}</p>
+                        </div>
+                    </div>
+                    ))}
                 </div>
             </div>
-            ))}
-        </div>
+        </section>
     )
 }
--- app/layout.tsx
+++ app/layout.tsx
@@ -3,6 +3,8 @@ import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 import Header from './components/header';
 import Footer from "./components/footer/footer";
+import Faq from "./components/faq/faq";
+import { FAQ_LIST } from "./constants/faq.constants";
 
 const geistSans = Geist({
   variable: "--font-geist-sans",
@@ -31,6 +33,7 @@ export default function RootLayout({
       >
         <Header />
         {children}
+        <Faq items={FAQ_LIST} />
         <Footer />
       </body>
     </html>
