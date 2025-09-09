bacaf500d3e698b12b93bc1768b71a64c01ddd7d
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-09-09 22:16:35 +0300

    chore(app): deploy

--- next.config.ts
+++ next.config.ts
@@ -2,6 +2,15 @@ import type { NextConfig } from "next";
 import { PAGE_COMPANY, PAGE_COMPANY_ABOUT, PAGE_PRODUCTS, PAGE_PRODUCT_ALL_IN_ONE } from "./app/constants/navigation.constants";
 
 const nextConfig: NextConfig = {
+  output: 'export',
+  compiler: {
+    removeConsole: process.env.NODE_ENV === 'production',
+  },
+  trailingSlash: true,
+  images: {
+    unoptimized: true // Обязательно для static export
+  },
+  basePath: process.env.NODE_ENV === 'production' ? '/yigim-app' : '',
   /* config options here */
   async redirects() {
     return [
