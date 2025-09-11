c4e34c78cfc1303cdf98c4ea67f3de927c3de1d6
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-09-11 22:38:31 +0300

    chore(app): add our vision section

---
+++ app/components/our-vision/our-vision.tsx
@@ -0,0 +1,16 @@
+'use client';
+
+import styles from './our-vision.module.css';
+
+export default function OurVision() {
+  return (
+    <section className={styles.section}>
+        <div className={styles.content}>
+            <div className={styles.text}>
+                <h3 className={styles.title}>Our vision</h3>
+                <p className={styles.description}>To become a company that creates the most efficient<br /> and the most human-centric payment systems.</p>
+            </div>
+        </div>
+    </section>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -2,12 +2,14 @@
 
 import KeyFacts from '@/components/key-facts/key-facts';
 import MainHeader from '@/components/main-header/main-header';
+import OurVision from './components/our-vision/our-vision';
 
 export default function HomePage() {
   return (
     <>
       <MainHeader />
       <KeyFacts />
+      <OurVision />
     </>
   );
 }
