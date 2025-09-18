6a3485904cc7973fea7c7988208cc4a52905dfa6
Фуад Джавадов <fuad.dzhavadov@idp.zyfra.com>
2025-09-19 01:17:07 +0300

    chore(partners): add feedbacks

--- app/company/partners/page.tsx
+++ app/company/partners/page.tsx
@@ -1,11 +1,12 @@
 'use client';
 
 import PageWrapper from '@/components/page-wrapper/page-wrapper';
+import WhoLovesUs from './components/who-loves-us/who-loves-us';
 
 export default function PartnersPage() {
   return (
     <PageWrapper title='Our partners'>
-        some content
+        <WhoLovesUs />
     </PageWrapper>
   );
 }
