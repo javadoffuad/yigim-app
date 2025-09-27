407c3c56427df1e2d89d5572080ece890e6c4681
javadoffuad <javadoffuad@gmail.com>
2025-09-27 20:59:40 +0300

    chore(app): add textarea component

--- app/(company)/contacts/components/contact-form/contact-form.tsx
+++ app/(company)/contacts/components/contact-form/contact-form.tsx
@@ -1,12 +1,18 @@
 'use client';
 
-import Image from 'next/image';
+import { useState } from 'react';
 import styles from './contact-form.module.css';
 import userIcon from '@/public/icons/user.svg';
 import phoneIcon from '@/public/icons/phone.svg';
 import letterIcon from '@/public/icons/letter.svg';
+import Input from '@/app/components/ui/input/input';
+import Textarea from '@/app/components/ui/textarea/textarea';
 
 export default function ContactForm() {
+    const [fullName, setFullName] = useState("");
+    const [phoneNumber, setPhoneNumber] = useState("");
+    const [message, setMessage] = useState("");
+
     return (
         <div className={styles["contact-form-section"]}>
             <div className={styles["form-header"]}>
@@ -16,21 +22,16 @@ export default function ContactForm() {
             
             <form className={styles["contact-form"]}>
                 <div className={styles["form-row"]}>
-                    <div className={styles["input-group"]}>
-                        <Image src={userIcon} className={styles["input-icon"]} alt='' />
-                        <input type="text" placeholder="Full Name" className={styles["form-input"]} />
-                    </div>
+                    <Input icon={userIcon} label="Full Name" value={fullName} onChange={setFullName} />
                     
-                    <div className={styles["input-group"]}>
-                        <Image src={phoneIcon} className={styles["input-icon"]} alt='' />
-                        <input type="tel" placeholder="Phone number" className={styles["form-input"]} />
-                    </div>
+                    <Input icon={phoneIcon} label="Phone number" value={phoneNumber} onChange={setPhoneNumber} />
                 </div>
-                
-                <div className={`${styles["input-group"]} ${styles["textarea-group"]}`}>
-                    <Image src={letterIcon} className={styles["input-icon"]} alt='' />
-                    <textarea placeholder="Message" className={styles["form-textarea"]}></textarea>
-                </div>
+
+                <Textarea
+                    icon={letterIcon}
+                    label="Message"
+                    value={message}
+                    onChange={setMessage} />
                 
                 <button type="submit" className={styles["submit-btn"]}>Send request</button>
             </form>
--- app/components/ui/textarea/textarea.tsx
+++ app/components/ui/textarea/textarea.tsx
@@ -1,32 +1,30 @@
 import React, { useState, useRef, useEffect } from 'react';
-import styles from './input.module.css';
+import styles from './textarea.module.css';
 import Image from 'next/image';
 
-interface InputProps {
+interface TextareaProps {
   label: string;
   value: string;
   onChange: (value: string) => void;
   icon: string;
-  type?: 'text' | 'password' | 'email';
   error?: string;
   caption?: string;
   required?: boolean;
   disabled?: boolean;
 }
 
-const Input: React.FC<InputProps> = ({
+const Textarea: React.FC<TextareaProps> = ({
   label,
   value,
   onChange,
   icon,
-  type = 'text',
   error,
   caption,
   required = false,
   disabled = false
 }) => {
   const [isFocused, setIsFocused] = useState(false);
-  const inputRef = useRef<HTMLInputElement>(null);
+  const inputRef = useRef<HTMLTextAreaElement>(null);
 
   const handleFocus = () => setIsFocused(true);
   const handleBlur = () => setIsFocused(false);
@@ -36,9 +34,8 @@ const Input: React.FC<InputProps> = ({
   return (
     <div className={`${styles['input-container']} ${error ? styles['input-error'] : ''} ${disabled ? styles['input-disabled'] : ''}`}>
       <div className={styles["input-wrapper"]}>
-        <input
+        <textarea
           ref={inputRef}
-          type={type}
           value={value}
           onChange={(e) => onChange(e.target.value)}
           onFocus={handleFocus}
@@ -46,7 +43,7 @@ const Input: React.FC<InputProps> = ({
           required={required}
           disabled={disabled}
           className={styles["input-field"]}
-        />
+        ></textarea>
 
         <Image src={icon} className={styles["input-icon"]} alt="" />
 
@@ -69,4 +66,4 @@ const Input: React.FC<InputProps> = ({
   );
 };
 
-export default Input;
+export default Textarea;
