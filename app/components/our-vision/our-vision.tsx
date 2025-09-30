8139d9a8b315b99d61b6ac7c5bf4df6cd188c3f3
javadoffuad <javadoffuad@gmail.com>
2025-10-01 00:47:35 +0300

    chore(Home): update i18n

--- app/components/our-vision/our-vision.tsx
+++ app/components/our-vision/our-vision.tsx
@@ -1,14 +1,19 @@
 'use client';
 
+import { useTranslations } from 'next-intl';
 import styles from './our-vision.module.css';
+import { I18N_HOME_PAGE } from '@/app/constants/i18n.constants';
 
 export default function OurVision() {
+  const t = useTranslations(`${I18N_HOME_PAGE}.AreaOurVision`);
   return (
     <section className={styles.section}>
         <div className={styles.content}>
             <div className={styles.text}>
-                <h3 className={styles.title}>Our vision</h3>
-                <p className={styles.description}>To become a company that creates the most efficient<br /> and the most human-centric payment systems.</p>
+                <h3 className={styles.title}>{t('Label')}</h3>
+                <p className={styles.description}>
+                  {t('Description')}
+                </p>
             </div>
         </div>
     </section>
