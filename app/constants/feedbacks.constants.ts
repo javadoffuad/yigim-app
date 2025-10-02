21f0b94c89cb8983aaaed9f7e9822b8bb997452e
javadoffuad <javadoffuad@gmail.com>
2025-10-02 21:21:59 +0300

    chore(Partners): update feedbacks const

--- app/constants/feedbacks.constants.ts
+++ app/constants/feedbacks.constants.ts
@@ -4,6 +4,9 @@ import yango from '@/public/partners/yango.svg';
 import turboaz from '@/public/partners/turbo.az.svg';
 import uber from '@/public/partners/uber.svg';
 import wingz from '@/public/partners/wingz.svg';
+import hilton from '@/public/partners/hilton.svg';
+import tapaz from '@/public/partners/tap.az.svg';
+import push30 from '@/public/partners/push30.svg';
 import { IFeedback } from '../models/feedback.models';
 
 const feedbackBolt: IFeedback = {
@@ -22,7 +25,7 @@ const feedbackWolt: IFeedback = {
     color: '#00C2E8',
   },
   author: 'Tofik Suleymanov',
-  feedback: 'YIĞIM truly understands...',
+  feedback: 'YIĞIM truly understands the unique needs of our business. Their collaborative approach and ability to tailor solutions specifically for WOLT Azerbaijan.',
   position: 'General Manager Wolt Azerbaijan'
 };
 
@@ -31,9 +34,9 @@ const feedbackUber: IFeedback = {
     icon: uber,
     color: '#010202',
   },
-  author: 'Elvin Mammadov',
-  feedback: "YIĞIM has a deep understanding of our specific business requirements. Their teamwork and tailored solutions have greatly benefited WOLT Azerbaijan.",
-  position: 'Uber Azerbaijan'
+  author: 'Mushvig Hasanov',
+  feedback: "YIĞIM has a deep understanding of our specific business requirements. Their teamwork and tailored solutions have greatly benefited Uber Azerbaijan.",
+  position: 'Regional Director - Uber'
 };
 
 const feedbackTurboaz: IFeedback = {
@@ -41,9 +44,9 @@ const feedbackTurboaz: IFeedback = {
     icon: turboaz,
     color: '#D8181F',
   },
-  author: 'Ramin Aliyev',
-  feedback: "YIĞIM's customized payment solutions have revolutionized operations for BOLT Azerbaijan, enhancing efficiency and customer satisfaction.",
-  position: 'Turbo.az'
+  author: 'Mamuka Kirvalidze',
+  feedback: "YIĞIM's customized payment solutions have revolutionized operations for Turbo.az, enhancing efficiency and customer satisfaction.",
+  position: 'CEO at Digital Classifieds'
 };
 
 const feedbackWingz: IFeedback = {
@@ -51,9 +54,29 @@ const feedbackWingz: IFeedback = {
     icon: wingz,
     color: '#FFF34C',
   },
-  author: 'Kamran Huseynov',
+  author: 'Timur Muravyov',
   feedback: "I value partners who prioritize discretion and provide seamless service. YIĞIM's tailored payment solutions exemplify elegance and efficiency.",
-  position: 'CFO Hilton Azerbaijan'
+  position: 'CEO Sakuraz Holding'
+};
+
+const feedbackHilton: IFeedback = {
+  brand: {
+    icon: hilton,
+    color: '#1E4A6E',
+  },
+  author: 'Aykut Korkmaz',
+  feedback: "I appreciate partners who understand discretion and seamless service. YIĞIM's bespoke payment solutions have been just that - elegant, efficient.",
+  position: 'CEO Hilton Azerbaijan'
+};
+
+const feedbackTapaz: IFeedback = {
+  brand: {
+    icon: tapaz,
+    color: '#FF4F08',
+  },
+  author: 'Mamuka Kirvalidze',
+  feedback: "YIĞIM comprehends the distinct needs of our enterprise. Their cooperative strategy and bespoke solutions have been invaluable for Tap.az.",
+  position: 'CEO at Digital Classifieds'
 };
 
 const feedbackYango: IFeedback = {
@@ -61,39 +84,51 @@ const feedbackYango: IFeedback = {
     icon: yango,
     color: '#FF0000',
   },
-  author: 'Ramiz Guliyev',
-  feedback: "I appreciate partners who prioritize discretion and deliver seamless service. YIĞIM's tailored payment solutions have been elegant and efficient.",
-  position: 'Yango'
+  author: 'Mushvig Hasanov',
+  feedback: "I appreciate partners who prioritize discretion and deliver seamless service. Yango's tailored payment solutions have been elegant and efficient.",
+  position: 'Regional Director - Yango'
+};
+
+const feedbackPush30: IFeedback = {
+  brand: {
+    icon: push30,
+    color: '#152A2A',
+  },
+  author: 'Adil Gasimov',
+  feedback: "YIĞIM's customized payment solutions have significantly improved Push30's operational capabilities.",
+  position: 'CEO Push30'
 };
 
 export const FEEDBACKS1: IFeedback[] = [
   feedbackWolt,
   feedbackYango,
-  feedbackUber,
   feedbackBolt,
   feedbackTurboaz,
-  feedbackWolt,
+  feedbackHilton,
   feedbackWingz,
   feedbackUber,
+  feedbackTapaz,
+  feedbackPush30,
 ];
 export const FEEDBACKS2: IFeedback[] = [
-  feedbackWingz,
   feedbackTurboaz,
-  feedbackUber,
-  feedbackWolt,
   feedbackWingz,
-  feedbackBolt,
-  feedbackYango,
+  feedbackTapaz,
+  feedbackWolt,
   feedbackUber,
+  feedbackYango,
+  feedbackPush30,
+  feedbackBolt,
+  feedbackHilton,
 ];
 export const FEEDBACKS3: IFeedback[] = [
-  feedbackWingz,
-  feedbackYango,
-  feedbackTurboaz,
+  feedbackPush30,
+  feedbackTapaz,
   feedbackUber,
-  feedbackBolt,
-  feedbackWingz,
   feedbackWolt,
+  feedbackYango,
   feedbackBolt,
-  feedbackUber,
+  feedbackHilton,
+  feedbackWingz,
+  feedbackTurboaz,
 ];
