bfa033f2d35d6e09ed2279b2cf9852e29076aa67
javadoffuad <javadoffuad@gmail.com>
2025-10-02 23:39:08 +0300

    chore(About): update i18n

--- app/[locale]/(company)/about/page.tsx
+++ app/[locale]/(company)/about/page.tsx
@@ -1,11 +1,14 @@
 'use client';
 
 import UnderConstruction from "@/app/components/ui/under-construction/under-construction";
+import { I18N_ABOUT_PAGE } from "@/app/constants/i18n.constants";
 import PageWrapper from "@/components/page-wrapper/page-wrapper";
+import { useTranslations } from "next-intl";
 
 export default function AboutPage() {
+  const t = useTranslations(`${I18N_ABOUT_PAGE}`);
   return (
-    <PageWrapper title='Behind the Brand'>
+    <PageWrapper title={t('Label')}>
       <UnderConstruction />
     </PageWrapper>
   );
--- app/constants/i18n.constants.ts
+++ app/constants/i18n.constants.ts
@@ -3,3 +3,4 @@ export const I18N_CONTACTS_PAGE = 'PageContacts';
 export const I18N_NEWS_PAGE = 'PageNews';
 export const I18N_API_PAGE = 'PageApi';
 export const I18N_HELP_PAGE = 'PageHelp';
+export const I18N_ABOUT_PAGE = 'PageAbout';
