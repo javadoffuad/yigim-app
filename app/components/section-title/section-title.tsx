fe2d1e23b0e3a114964f5201c715d2bf174fe992
javadoffuad <javadoffuad@gmail.com>
2025-10-04 23:31:48 +0300

    chore(App): section title support different size

--- app/components/section-title/section-title.tsx
+++ app/components/section-title/section-title.tsx
@@ -8,14 +8,16 @@ interface Props {
     title: string;
     description: string | TrustedHTML;
     textAlign?: 'left' | 'center';
+    size?: 'm' | 'l';
 }
 
-export default function SectionTitle({title, description, textAlign}: Props) {
+export default function SectionTitle({title, description, textAlign, size}: Props) {
   const t = useTranslations(`${I18N_PARTNERS_PAGE}`);
   const align = textAlign === 'left' ? styles['align-left'] : '';
+  const textSize = size === 'm' ? styles['middle'] : '';
 
   return (
-    <div className={`${styles.container} ${align}`}>
+    <div className={`${styles.container} ${align} ${textSize}`}>
         <h3 className={styles.title}>{title}</h3>
         <p className={styles.description} dangerouslySetInnerHTML={{__html: description}} />
     </div>
