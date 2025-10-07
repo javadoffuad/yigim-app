512731613347f403680b2db9c28e692729886111
javadoffuad <javadoffuad@gmail.com>
2025-10-08 01:16:16 +0300

    chore(App): update terms pages

--- app/[locale]/(resources)/policy/page.tsx
+++ app/[locale]/(resources)/policy/page.tsx
@@ -1,15 +1,15 @@
 'use client';
 
-import UnderConstruction from "@/app/components/ui/under-construction/under-construction";
-import { I18N_COOKİE_POLICY_PAGE } from "@/app/constants/i18n.constants";
+import { I18N_COOKIE_POLICY_PAGE } from "@/app/constants/i18n.constants";
 import PageWrapper from "@/components/page-wrapper/page-wrapper";
 import { useTranslations } from "next-intl";
+import styles from './page.module.css';
 
 export default function HelpCenterPage() {
-  const t = useTranslations(I18N_COOKİE_POLICY_PAGE);
+  const t = useTranslations(I18N_COOKIE_POLICY_PAGE);
   return (
     <PageWrapper title={t('Label')}>
-      <UnderConstruction />
+      <div className={styles.content} dangerouslySetInnerHTML={{__html: t.raw('Content')}} />
     </PageWrapper>
   );
 }
