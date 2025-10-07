01dab9010727dc1b3db6d265eb0b46c1619f4702
javadoffuad <javadoffuad@gmail.com>
2025-10-08 00:07:23 +0300

    chore(App): update footer

--- app/components/footer/footer.tsx
+++ app/components/footer/footer.tsx
@@ -7,7 +7,7 @@ import fb from '@/public/social/fb-logo.svg';
 import instagram from '@/public/social/instagram-logo.svg';
 import linkedin from '@/public/social/linkedin-logo.svg';
 import youtube from '@/public/social/youtube-logo.svg';
-import {Link} from '@/i18n/navigation';
+import { Link } from '@/i18n/navigation';
 import { PAGE_COMPANY_ABOUT, PAGE_COMPANY_CONTACTS, PAGE_COMPANY_NEWS, PAGE_COMPANY_PARTNERS, PAGE_POLICIY, PAGE_PRODUCT_ALL_IN_ONE, PAGE_PRODUCT_FAST_INVOICE_PAYMENT, PAGE_PRODUCT_INTERNET_ACQUIRING, PAGE_PRODUCT_LINK_PAYMENT, PAGE_RESOURCES_DEVELOPERS, PAGE_RESOURCES_HELP_CENTER, PAGE_SECURITY, PAGE_SOCIAL_FACEBOOK, PAGE_SOCIAL_INSTAGRAM, PAGE_SOCIAL_LINKEDIN, PAGE_SOCIAL_YOUTUBE, PAGE_TERMS_CONDITION } from '@/app/constants/navigation.constants';
 import FooterColumn from '../footer-column/footer-column';
 import { useTranslations } from 'next-intl';
@@ -74,48 +74,49 @@ export default function Footer() {
 
     return (
         <footer className={styles["footer"]}>
-        <div className={styles["footer-content"]}>
-            <div className={styles["footer-top"]}>
-                <Image src={logo} alt="YIGIM" className={styles["footer-logo"]} />
-                
-                <div className={styles["footer-links"]}>
-                    <FooterColumn label={t('AreaNavigations.Products.Label')} links={products}/>
-                    <FooterColumn label={t('AreaNavigations.Company.Label')} links={company}/>
-                    <FooterColumn label={t('AreaNavigations.Resources.Label')} links={resources}/>
+            <div className={styles["footer-content"]}>
+                <div className={styles["footer-top"]}>
+                    <div className={styles["footer-social-container"]}>
+                        <Image src={logo} alt="YIGIM" className={styles["footer-logo"]} />
+
+                        <div className={styles["social-pane"]}>
+                            <p>Social media</p>
+                            <div className={styles["social-links"]}>
+                                <Link href={PAGE_SOCIAL_FACEBOOK} className={styles["social-link"]} target='_blank'>
+                                    <Image src={fb} alt="Facebook" />
+                                </Link>
+                                <Link href={PAGE_SOCIAL_INSTAGRAM} className={styles["social-link"]} target='_blank'>
+                                    <Image src={instagram} alt="Instagram" />
+                                </Link>
+                                <Link href={PAGE_SOCIAL_LINKEDIN} className={styles["social-link"]} target='_blank'>
+                                    <Image src={linkedin} alt="LinkedIn" />
+                                </Link>
+                                <Link href={PAGE_SOCIAL_YOUTUBE} className={styles["social-link"]} target='_blank'>
+                                    <Image src={youtube} alt="YouTube" />
+                                </Link>
+                            </div>
+                        </div>
+                    </div>
+
+                    <div className={styles["footer-links"]}>
+                        <FooterColumn label={t('AreaNavigations.Products.Label')} links={products} />
+                        <FooterColumn label={t('AreaNavigations.Company.Label')} links={company} />
+                        <FooterColumn label={t('AreaNavigations.Resources.Label')} links={resources} />
+                    </div>
+                </div>
+
+                <div className={styles["footer-bottom"]}>
+                    <div className={styles["copyright"]}>© COPYRIGHT YIGIM 2025</div>
+                    <div className={styles["footer-legal"]}>
+                        <Link href={PAGE_SECURITY}>{t('AreaStaticNavigation.LabelSecurity')}</Link>
+                        <Link href={PAGE_POLICIY}>{t('AreaStaticNavigation.LabelPolicy')}</Link>
+                        <Link href={PAGE_TERMS_CONDITION}>{t('AreaStaticNavigation.LabelTerms')}</Link>
+                    </div>
+                    <button className={styles["back-to-top"]} onClick={scrollToTop}>
+                        {t('AreaStaticNavigation.LabelBackToTop')}
+                    </button>
                 </div>
             </div>
-            
-            <div className={styles["footer-social"]}>
-                <Link href={PAGE_SOCIAL_FACEBOOK} className={styles["social-item"]} target='_blank'>
-                    <Image src={fb} alt="Facebook" />
-                    <span>Facebook</span>
-                </Link>
-                <Link href={PAGE_SOCIAL_INSTAGRAM} className={styles["social-item"]} target='_blank'>
-                    <Image src={instagram} alt="Instagram" />
-                    <span>Instagram</span>
-                </Link>
-                <Link href={PAGE_SOCIAL_LINKEDIN} className={styles["social-item"]} target='_blank'>
-                    <Image src={linkedin} alt="LinkedIn" />
-                    <span>Linkedin</span>
-                </Link>
-                <Link href={PAGE_SOCIAL_YOUTUBE} className={styles["social-item"]} target='_blank'>
-                    <Image src={youtube} alt="YouTube" />
-                    <span>Youtube</span>
-                </Link>
-            </div>
-            
-            <div className={styles["footer-bottom"]}>
-                <div className={styles["copyright"]}>© COPYRIGHT YIGIM 2025</div>
-                <div className={styles["footer-legal"]}>
-                    <Link href={PAGE_SECURITY}>{t('AreaStaticNavigation.LabelSecurity')}</Link>
-                    <Link href={PAGE_POLICIY}>{t('AreaStaticNavigation.LabelPolicy')}</Link>
-                    <Link href={PAGE_TERMS_CONDITION}>{t('AreaStaticNavigation.LabelTerms')}</Link>
-                </div>
-                <div className={styles["back-to-top"]} onClick={scrollToTop}>
-                    {t('AreaStaticNavigation.LabelBackToTop')} ↑
-                </div>
-            </div>
-        </div>
-    </footer>
+        </footer>
     )
 }
