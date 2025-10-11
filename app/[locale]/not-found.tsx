bb7d3b0737696501960aacea12076cca032b7a50
javadoffuad <javadoffuad@gmail.com>
2025-10-11 18:56:59 +0300

    chore(App): update 404 page

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,19 +1,20 @@
-'use client';
-
 import Header from '@/components/header/header';
 import { RequestCallback } from '@/components/request-callback/request-callback';
 import ErrorPageWrapper from '@/components/error-page-wrapper/error-page-wrapper';
 import { NextIntlClientProvider } from 'next-intl';
-import messagesEn from '@/messages/en.json';
+import { getMessages } from 'next-intl/server';
+import Footer from '../components/footer/footer';
 
-export default function GlobalNotFound() {
+export default async function NotFound() {
+  const messages = await getMessages();
   return (
     <html lang="en">
       <body>
-        <NextIntlClientProvider locale="en" messages={messagesEn}>
+        <NextIntlClientProvider messages={messages}>
           <Header />
           <RequestCallback />
           <ErrorPageWrapper errorCode="400" />
+          <Footer />
         </NextIntlClientProvider>
       </body>
     </html>
--- app/components/error-page-wrapper/error-page-wrapper.tsx
+++ app/components/error-page-wrapper/error-page-wrapper.tsx
@@ -13,11 +13,13 @@ interface ErrorPageProps {
 export default function ErrorPageWrapper({errorCode}: ErrorPageProps) {
   const t = useTranslations(I18N_NOT_FOUND_PAGE);
   return (
-    <div className={styles.bg}>
-      <div className={styles.container}>
-        <p className={styles.message}>{t('Label')}</p>
-        <h1 className={styles.code}>{errorCode}</h1>
-        <Link href={PAGE_HOME} className="button button-primary">{t('ButtonGoHome.Label')}</Link>
+    <div className={styles.page}>
+      <div className={styles.bg}>
+        <div className={styles.container}>
+          <p className={styles.message}>{t('Label')}</p>
+          <h1 className={styles.code}>{errorCode}</h1>
+          <Link href={PAGE_HOME} className="button button-primary">{t('ButtonGoHome.Label')}</Link>
+        </div>
       </div>
     </div>
   );
