7af713eaecbdadc08b54f0b68690c109904cbe0e
javadoffuad <javadoffuad@gmail.com>
2025-10-15 19:17:11 +0300

    chore(Home) 🔍️ seo optiomization

--- app/[locale]/(main)/layout.tsx
+++ app/[locale]/(main)/layout.tsx
@@ -6,11 +6,19 @@ import { hasLocale, NextIntlClientProvider } from "next-intl";
 import { notFound } from "next/navigation";
 import { routing } from "@/i18n/routing";
 import { ILayoutProps } from "@/app/models/layout.models";
+import { getTranslations } from "next-intl/server";
+import { I18N_HOME_PAGE } from "@/app/constants/i18n.constants";
 
-export const metadata: Metadata = {
-  title: "Yigim Home page",
-  description: "Yigim Home page description",
-};
+export async function generateMetadata({params}: ILayoutProps) {
+  const {locale} = await params;
+  const t = await getTranslations({locale, namespace: I18N_HOME_PAGE});
+ 
+  return {
+    title: t('Metadata.Title'),
+    description: t('Metadata.Description'),
+    keywords: t('Metadata.Keywords')
+  } as Metadata;
+}
 
 export default async function MainLayout({children, params}: ILayoutProps) {
   // Ensure that the incoming `locale` is valid
