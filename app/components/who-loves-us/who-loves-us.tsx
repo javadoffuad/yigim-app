430c503ae147085237458bd384bda003882c7166
javadoffuad <javadoffuad@gmail.com>
2025-10-22 23:27:50 +0300

    feat(Home): 💄 make page responsive

--- app/components/who-loves-us/who-loves-us.tsx
+++ app/components/who-loves-us/who-loves-us.tsx
@@ -6,6 +6,7 @@ import styles from './who-loves-us.module.css';
 import { useTranslations } from 'next-intl';
 import { I18N_HOME_PAGE } from '@/app/constants/i18n.constants';
 import SectionTitle from '../section-title/section-title';
+import { useWindowSize } from '@/app/hooks/use-window-size';
 
 const feedbacks1 = [
   ...FEEDBACKS1,
@@ -27,6 +28,20 @@ const feedbacks3 = [
 
 export default function WhoLovesUs() {
   const t = useTranslations(`${I18N_HOME_PAGE}.AreaWhoLovesUs`);
+  const {width} = useWindowSize();
+
+  if (width === 0) {
+    return (
+      <section className={styles.section}>
+        <div className={styles.content}>
+          <SectionTitle
+            title={t('Label')}
+            description={t('DescriptionClient')} />
+        </div>
+      </section>
+    )
+  }
+
   return (
     <section className={styles.section}>
         <div className={styles.content}>
@@ -42,20 +57,30 @@ export default function WhoLovesUs() {
                   }
                 </div>
               </div>
-              <div className={styles.column}>
-                <div className={`${styles["scroll-content"]} ${styles["scroll-up"]}`}>
-                  {
-                    feedbacks2.map((feedback, index) => <FeedbackCard key={index} {...feedback} />)
-                  }
-                </div>
-              </div>
-              <div className={styles.column}>
-                <div className={`${styles["scroll-content"]} ${styles["scroll-down"]}`}>
-                  {
-                    feedbacks3.map((feedback, index) => <FeedbackCard key={index} {...feedback} />)
-                  }
-                </div>
-              </div>
+
+              {
+                width > 767
+                  ? <div className={styles.column}>
+                      <div className={`${styles["scroll-content"]} ${styles["scroll-up"]}`}>
+                        {
+                          feedbacks2.map((feedback, index) => <FeedbackCard key={index} {...feedback} />)
+                        }
+                      </div>
+                    </div>
+                  : null
+              }
+              
+              {
+                width > 1023
+                  ? <div className={styles.column}>
+                      <div className={`${styles["scroll-content"]} ${styles["scroll-down"]}`}>
+                        {
+                          feedbacks3.map((feedback, index) => <FeedbackCard key={index} {...feedback} />)
+                        }
+                      </div>
+                    </div>
+                  : null
+              }
             </div>
         </div>
     </section>
