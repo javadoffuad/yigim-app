42650d43d92c49f77321ea114b17357627cd6ffe
javadoffuad <javadoffuad@gmail.com>
2025-10-29 00:46:11 +0300

    fix(App): 🐛 carousel responsive on resize

--- app/components/carousel-photos/carousel-photos.tsx
+++ app/components/carousel-photos/carousel-photos.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import styles from "./carousel-photos.module.css";
 import Slider from "react-slick";
@@ -9,55 +11,71 @@ import carousel4 from '@/public/images/carousel-4.png';
 import carousel5 from '@/public/images/carousel-5.png';
 import "slick-carousel/slick/slick.css";
 import "slick-carousel/slick/slick-theme.css";
+import { useWindowSize } from "@/app/hooks/use-window-size";
 
 export default function CarouselPhotos() {
-  const settings = {
-    dots: false,
-    infinite: true,
-    speed: 500,
-    slidesToShow: 4,
-    slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 2000,
-    cssEase: "linear",
-    swipeToSlide: true,
-    arrows: false,
-    centerMode: true,
-    centerPadding: '100px',
-    responsive: [
-      {
-        breakpoint: 1023,
-        settings: {
-          slidesToShow: 3,
-          slidesToScroll: 3,
-          initialSlide: 3,
-          centerPadding: '60px',
-        }
-      },
-      {
-        breakpoint: 767,
-        settings: {
-          slidesToShow: 2,
-          slidesToScroll: 2,
-          initialSlide: 2,
-          centerPadding: '40px',
-        }
-      },
-      {
-        breakpoint: 560,
-        settings: {
-          slidesToShow: 1,
-          slidesToScroll: 1,
-          initialSlide: 1,
-          centerPadding: '20px',
-        }
+  const { width } = useWindowSize();
+
+  const getSettings = () => {
+    const baseSettings = {
+      ddots: false,
+      infinite: true,
+      speed: 500,
+      slidesToShow: 4,
+      slidesToScroll: 1,
+      autoplay: true,
+      autoplaySpeed: 2000,
+      cssEase: "linear",
+      swipeToSlide: true,
+      arrows: false,
+      centerMode: true,
+      centerPadding: '100px',
+    }
+
+    if (width <= 560) {
+      return {
+        ...baseSettings,
+        slidesToShow: 1,
+        slidesToScroll: 1,
+        initialSlide: 1,
+        centerPadding: '20px',
       }
-    ]
-  };
+    } else if (width <= 767) {
+      return {
+        ...baseSettings,
+        slidesToShow: 2,
+        slidesToScroll: 2,
+        initialSlide: 2,
+        centerPadding: '40px',
+      }
+    } else if (width <= 1023) {
+      return {
+        ...baseSettings,
+        slidesToShow: 3,
+        slidesToScroll: 3,
+        initialSlide: 3,
+        centerPadding: '60px',
+      }
+    } else {
+      return {
+        ...baseSettings,
+        slidesToShow: 4,
+        slidesToScroll: 1,
+        initialSlide: 4,
+        centerPadding: '100px',
+      }
+    }
+  }
+
+  if (width === 0) {
+    return (
+      <div className={styles.carousel} />
+    )
+  }
 
   return (
     <div className={styles.carousel}>
-      <Slider {...settings}>
+      <Slider {...getSettings()}>
         <div className={styles["carousel-item"]}>
           <Image src={carousel1} alt="" />
         </div>
--- app/components/our-team/our-team.tsx
+++ app/components/our-team/our-team.tsx
@@ -1,3 +1,5 @@
+'use client';
+
 import React from "react";
 import styles from "./our-team.module.css";
 import { useTranslations } from "next-intl";
