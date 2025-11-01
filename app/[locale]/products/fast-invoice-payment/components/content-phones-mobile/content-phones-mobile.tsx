bb3303df089620f0676a258baa689bc27864b837
javadoffuad <javadoffuad@gmail.com>
2025-11-01 21:41:13 +0300

    feat(Products): 💫 Phone tooltip animation

--- app/[locale]/products/fast-invoice-payment/components/content-phones-mobile/content-phones-mobile.tsx
+++ app/[locale]/products/fast-invoice-payment/components/content-phones-mobile/content-phones-mobile.tsx
@@ -1,6 +1,7 @@
 import React, { useState, useEffect, useCallback } from 'react';
 import Image, { StaticImageData } from 'next/image';
 import styles from './content-phones-mobile.module.css';
+import Tooltip from '@/app/components/ui/tooltip/tooltip';
 
 const ContentPhonesMobile = ({ phones }: { phones: { tooltip: string, icon: StaticImageData }[] }) => {
   const [cards, setCards] = useState(phones);
@@ -44,10 +45,11 @@ const ContentPhonesMobile = ({ phones }: { phones: { tooltip: string, icon: Stat
               ${styles["card"]} 
               ${animating && index === 0 ? styles['slide-out'] : ''}
               ${animating && (index > 0) ? styles['slide-' + index] : ''}
-              // ${animating && index === cards.length - 1 ? styles['slide-in'] : ''}
+              ${animating && (index === cards.length - 1) ? styles['slide-in'] : ''}
             `}
           >
-            <Image src={card.icon} alt={card.tooltip} />
+            <Tooltip className={styles["tooltip"]} text={card.tooltip} />
+            <Image className={styles.image} src={card.icon} alt={card.tooltip} />
           </div>
         ))}
       </div>
--- app/components/ui/tooltip/tooltip.tsx
+++ app/components/ui/tooltip/tooltip.tsx
@@ -4,11 +4,12 @@ import styles from "./tooltip.module.css";
 
 interface TooltipProps {
     text: string;
+    className?: string;
 }
 
-export default function Tooltip({text}: TooltipProps) {
+export default function Tooltip({text, className}: TooltipProps) {
     return (
-        <div className={styles.tooltip}>
+        <div className={`${styles.tooltip} ${className ?? ''}`}>
             <span className={styles.text} dangerouslySetInnerHTML={{__html: text}} />
         </div>
     );
